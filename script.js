f4a1e01abef3969a4544259d9a10a77b7066c469
Aslı Kasap <72225773+AsliKasap@users.noreply.github.com>
2021-03-01 22:32:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+let timeUp = false;
+let score = 0;
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+function randomHole(holes) {
+  const holeIndex = Math.floor(Math.random() * holes.length);
+  const hole = holes[holeIndex];
+
+  if (hole === lastHole) {
+   
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(500, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep(); 
+  }, time);
+}
+function startGame() {
+  scoreBoard.innerHTML = 0;
+  timeUp = false;
+  if(localStorage.getItem("high")){
+      document.querySelector(".highscore").innerHTML="High Score ="+ localStorage.getItem("high");
+      
+  }else{
+    document.querySelector(".highscore").innerHTML="High Score = 0";
+  }
+  score = 0;
+  peep();
+  document.querySelector("button").style.visibility = "hidden";
+  gameOver();
+}
+function gameOver() {
+  setTimeout(() => {
+    document.querySelector("button").style.visibility = "visible";
+    timeUp = true;
+    if(localStorage.getItem("high")<score){
+        setScoreLS(score);
+    }
+  }, 10000); 
+}
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  lastHole.classList.remove("up");
+  scoreBoard.innerHTML = score;
+}
+function setScoreLS(score) {
+    localStorage.setItem("high",score);
+}
+
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
